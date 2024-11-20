9a10d26a5f2db80ed03175c37065f8cfd6a1d5bf
matias406042 <142633399+matias406042@users.noreply.github.com>
2024-11-20 04:15:18 -0300

    icons

--- src/app/inventories/iep-inventory/components/iep-inventory/iep-inventory.component.ts
+++ src/app/inventories/iep-inventory/components/iep-inventory/iep-inventory.component.ts
@@ -468,6 +468,7 @@ applyFilter(): void {
         {
           data: 'minQuantityWarning',
           title: 'Min. Alerta',
+          className: 'text-end',
         },
         {
           data: null,
